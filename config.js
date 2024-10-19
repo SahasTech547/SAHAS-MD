d22cfa818f6dd84a2797071a9d8224b8a99cd338
Sahas Tech <sahastech547@gmail.com>
2024-10-19 17:04:32 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "NHcDwITD#guDIpaLiZcgArJkmFm-wS3_-zoWN9lPW6hQ3rsrs0as",
+SESSION_ID: process.env.SESSION_ID || "TLRXkKja#Q9AN-A22sjmMaYqvWrxstfcivYxEuMntSQ9OePhc_yU",
 MONGODB: process.env.MONGODB || "mongodb+srv://kulathungaasitha319:yjHB0DvFfStNfwPS@cluster0.3oijd.mongodb.net/",
 ALIVE_IMG: process.env.ALIVE_IMG || "https://iili.io/dbFAKoG.jpg"
 };
