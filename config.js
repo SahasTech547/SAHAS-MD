42697a4e4311f18f91933fffb867c07ad2c260a7
Sahas Tech <sahastech547@gmail.com>
2024-10-20 09:53:26 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "82NzlYQZ#bffgNnJRpDFEXUhrtbPC6YmyAeHgsev0aU_97_DjMMc",
+SESSION_ID: process.env.SESSION_ID || "HPYCGCbC#kgdBxWyqX3Fk76oISEsHtTkcUXbkfIMGC8B1QitvwGU",
 MONGODB: process.env.MONGODB || "mongodb+srv://kulathungaasitha319:yjHB0DvFfStNfwPS@cluster0.3oijd.mongodb.net/",
 ALIVE_IMG: process.env.ALIVE_IMG || "https://iili.io/dbFAKoG.jpg",
 OMDB_API_KEY: process.env.OMDB_API_KEY || "76cb7f39"
